a40211bb49b0339e5a65e24b2a954fb57950cd9a
kuffintea <121694607+kuffintea@users.noreply.github.com>
2024-09-03 12:08:21 +0400

    Добавляет адаптивную сетку (#6)
    * Добавляет адаптивную сетку
    
    * Вносит изменения в сетку
    
    * Вносит мелкие изменения

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const mb = document.querySelectorAll('.main-header__toggle');
+const m = document.querySelector('.main-nav');
+
+document.querySelector('.main-header--nojs').classList.remove('main-header--nojs');
+
+for (const i of mb) {
+  i.onclick = function() {
+    m.classList.toggle('main-nav--show');
+  };
+}
