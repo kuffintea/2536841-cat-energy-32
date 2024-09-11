8877029569dcd1fc0c569d6f874838587e4d6c64
kuffintea <kris_al2005@mail.ru>
2024-09-11 13:08:01 +0300

    Вносит изменения согласно критериям

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,10 @@
 const mb = document.querySelectorAll('.main-header__toggle');
-const m = document.querySelector('.main-nav');
+const m = document.querySelector('.main-header__nav');
 
 document.querySelector('.main-header--nojs').classList.remove('main-header--nojs');
 
 for (const i of mb) {
   i.onclick = function() {
-    m.classList.toggle('main-nav--show');
+    m.classList.toggle('main-header__nav--show');
   };
 }
